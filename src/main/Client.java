5f3c1819bd2c9c3dc64c4faacf45c6e84e3aedba
Danry Ague <74456102+epcraft93@users.noreply.github.com>
2021-05-11 12:28:38 +0800

    Major update
    Now supports comprehensive checking of arguments and other input
    checking. If no arguments are present, the user will be manually asked
    on runtime.

--- src/main/Client.java
+++ src/main/Client.java
@@ -10,15 +10,15 @@ import java.net.Socket;
 /**
  * Simple implementation of client-server communication for the client-side
  * interface. When run in the command line, it accepts two arguments,
- * {@code address} and {@code port}. When not specified, it will default to
- * {@code 127.0.0.1:6013}. Runs indefinitely until the {@code exit} keyword is
- * received from the server.
+ * {@code address} and {@code port}. When not specified, the program will ask
+ * for input while running, otherwise it will default to {@code 127.0.0.1:6013}.
+ * Runs indefinitely until the {@code exit} keyword is received from the server.
  * 
  * <p>
  * Example console execute: <blockquote>
  * 
  * <pre>
- * java -jar chatbot-client.jar 127.0.0.1 5500
+ * java -jar chatbot-client.jar --address 127.0.0.1 -p 5500
  * </pre>
  * 
  * </blockquote>
@@ -33,16 +33,25 @@ import java.net.Socket;
  * <p>
  * 
  * @author Danry Ague
- * @version 2.3.7
+ * @version 2.6.4-alpha
  */
 public class Client {
+
+    /**
+     * Default address.
+     */
+    final static String DEF_LOCALHOST = "127.0.0.1";
+
+    /**
+     * Default port.
+     */
+    final static String DEF_PORT = "6013";
 
     /**
      * Used to indicate/prefix error logs. Must be concatenated with the actual
      * error message.
      */
     final static String PREF_ERR = "[ERROR] ";
-
     /**
      * Used to indicate/prefix informational logs. Must be concatenated with the
      * actual information message.
@@ -58,8 +67,8 @@ public class Client {
      * Clears the command line interface.
      */
     static void clearScreen() {
-	System.out.print("\033[H\033[2J");
-	System.out.flush();
+	stdStream.print("\033[H\033[2J");
+	stdStream.flush();
     }
 
     /**
@@ -80,7 +89,9 @@ public class Client {
 	BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
 	String reply;
 
-	/* Output current client instance URI and port when successfully connected */
+	clearScreen();
+
+	/** Output current client instance URI and port when successfully connected **/
 	stdStream.println(PREF_INF + "(Client connected to " + uri + ":" + port + ")" + "\n");
 
 	while ((reply = received.readLine()) != null) {
@@ -123,6 +134,20 @@ public class Client {
     }
 
     /**
+     * Provides a delay in milliseconds. Useful for making sure that the user sees
+     * an output before it is cleared.
+     * 
+     * @param ms desired delay in milliseconds
+     */
+    static void delay(int ms) {
+	try {
+	    Thread.sleep(ms);
+	} catch (InterruptedException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    /**
      * Generates a timestamp to be used.
      * 
      * @return a generated {@code String} timestamp
@@ -132,31 +157,125 @@ public class Client {
     }
 
     /**
+     * Used to get input from the user.
+     * 
+     * @param inputType The type of parameter to be acquired from the user
+     * @return The acquired input from the user
+     * @throws IOException
+     */
+    static String getInput(String inputType) {
+	BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+	String acquiredParam = null;
+
+	switch (inputType) {
+	case "address":
+	    stdStream.println("Address of the server: (leave blank for default)");
+	    try {
+		acquiredParam = consoleReader.readLine();
+		if (acquiredParam.isBlank()) {
+		    acquiredParam = DEF_LOCALHOST;
+		} else if (!isValidAddress(acquiredParam)) {
+		    stdStream.println("Invalid address, using defaults." + "\n");
+		    acquiredParam = DEF_LOCALHOST;
+		}
+	    } catch (IOException e) {
+		stdStream.println("There was an error in receiving input data.");
+		e.printStackTrace();
+	    }
+	    break;
+	case "port":
+	    stdStream.println("Port of the server: (leave blank for default)");
+	    try {
+		acquiredParam = consoleReader.readLine();
+		if (acquiredParam.isBlank()) {
+		    acquiredParam = DEF_PORT;
+		}
+	    } catch (IOException e) {
+		stdStream.println("There was an error in receiving input data.");
+		e.printStackTrace();
+	    }
+	    break;
+	}
+
+	return acquiredParam;
+    }
+
+    /**
+     * Checks for address validity.
+     * 
+     * @param address the address to be checked
+     * @return {@code true} if address is valid
+     */
+    static boolean isValidAddress(String address) {
+	boolean flag = false;
+	int count = 0;
+	for (int i = 0; i < address.length(); i++) {
+	    if (address.charAt(i) == '.') {
+		count++;
+	    }
+	}
+	if (count == 3)
+	    flag = true;
+	return flag;
+    }
+
+    /**
      * Main entry point of the program. Clears the command line interface first,
-     * then checks if arguments are present before connecting to the server.
+     * then checks if arguments are present before continuing.
      * 
      * @param args (optional) address and port
      */
     public static void main(String[] args) {
 	clearScreen();
 
-	int port;
-	String address;
+	/** Defaults **/
+	int port = Integer.parseInt(DEF_PORT);
+	String address = DEF_LOCALHOST;
 
+	/** Check for arguments **/
 	if (args.length != 0) {
-	    address = args[0];
-	    port = Integer.parseInt(args[1]);
+	    for (int i = 0; i < args.length; i++) {
+		switch (args[i]) {
+		case "--address":
+		case "-a":
+		    if (isValidAddress(args[i + 1])) {
+			address = args[i + 1];
+		    } else {
+			stdStream.println("Invalid address, using defaults.");
+			delay(1000);
+		    }
+		    i++;
+		    break;
+		case "--port":
+		case "-p":
+		    try {
+			port = Integer.parseInt(args[i + 1]);
+		    } catch (NumberFormatException e) {
+			stdStream.println("Invalid port, using defaults.");
+			delay(1000);
+		    }
+		    i++;
+		    break;
+		default:
+		    stdStream.println("Unknown command: " + args[i]);
+		    i++;
+		}
+	    }
 	} else {
-	    /** Defaults, if there no console arguments **/
-	    address = "127.0.0.1";
-	    port = 6013;
+
+	    /* No arguments, ask for user input */
+	    address = getInput("address");
+	    try {
+		port = Integer.parseInt(getInput("port"));
+	    } catch (NumberFormatException e) {
+		stdStream.println("Invalid port, using defaults." + "\n");
+	    }
 	}
 
-	// TODO: Handle args and exceptions
 	try {
 	    connect(address, port);
 	} catch (IOException e) {
-	    stdStream.println(PREF_ERR + e.getMessage() + ".");
+	    stdStream.println(PREF_ERR + "@" + address + ":" + port + " | " + e.getMessage().toUpperCase() + ".");
 	}
 	stdStream.close();
     }
